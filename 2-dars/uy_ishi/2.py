6db43aa6043ff257afb164424f2e8749664c10a2
Anorqulov14 <eldoranorqulov@gmail.com>
2025-05-24 20:11:16 +0500

    sinf

---
+++ 2-dars/uy_ishi/2.py
@@ -0,0 +1,8 @@
+son = 5
+ustun = 5
+for i in range(1,11):
+    for j in range(1,ustun+1):    
+        if ustun == 3:
+            print()     
+        print(f"{j} * {i} = {i * j}", end="\t")
+    print()
